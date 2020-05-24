794448e899dd875da3c976fe716e66ac176ae373
Natalia- W158 <you@example.com>
2020-05-24 09:07:06 +0200

    suspects

---
+++ assets/js/main.js
@@ -0,0 +1,56 @@
+suspectsArray = [
+    {
+        firstName: "Jacob", 
+        lastName: "Green",
+        occupation: "Entrepreneur",
+        age: "45",
+        description: "He has a lot of connections",
+        color: "green", 
+
+},
+{
+    firstName: "Doctor", 
+    lastName: "Orchid",
+    occupation: "Scientist",
+    age: "26",
+    description: "PhD in plant toxicology. Adopted daughter of Mr. Boddy",
+    color: "white", 
+
+},
+{
+    firstName: "Victor", 
+    lastName: "Plum",
+    occupation: "Designer",
+    age: "22",
+    description: "Billionaire video game designer",
+    color: "purple", 
+
+},
+{
+    firstName: "Kasandra", 
+    lastName: "Scarlet",
+    occupation: "Actress",
+    age: "31",
+    description: "She is an A-list movie star with a dark past",
+    color: "red", 
+
+},
+{
+    firstName: "Eleanor", 
+    lastName: "Peacock",
+    occupation: "Socialité",
+    age: "36",
+    description: "She is from a wealthy family and uses her status and money to earn popularity",
+    color: "blue", 
+
+},
+{
+    firstName: "Jack", 
+    lastName: "Mustard",
+    occupation: "Retired Football player ",
+    age: "62",
+    description: "He is a former football player who tries to get by on his former glory",
+    color: "yellow", 
+
+},
+];
